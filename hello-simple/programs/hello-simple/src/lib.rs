3a670b265e3d6f6bbf5a2f88ee6d67a5e12856dc
smallmenu <smallmenu@gmail.com>
2026-02-06 10:07:36 +0800

    init

--- hello-simple/programs/hello-simple/src/lib.rs
+++ hello-simple/programs/hello-simple/src/lib.rs
@@ -8,6 +8,24 @@ pub mod hello_simple {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        
+        // 声明变量
+        let a: i32 = 10;
+        let b: i32 = 3;
+        
+        // 执行四则运算
+        let add_result = a + b;
+        let sub_result = a - b;
+        let mul_result = a * b;
+        let div_result = a / b;
+        
+        // 输出日志
+        msg!("Var a: {}, Var b: {}", a, b);
+        msg!("Add: {} + {} = {}", a, b, add_result);
+        msg!("Sub: {} - {} = {}", a, b, sub_result);
+        msg!("Mul: {} * {} = {}", a, b, mul_result);
+        msg!("Div: {} / {} = {}", a, b, div_result);
+        
         Ok(())
     }
 }
